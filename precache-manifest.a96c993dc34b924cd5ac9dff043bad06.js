c566e62dc14f8c23664d8a0acbfee3c6ff63d486
DevAshleyD <webdevmvp@gmail.com>
2019-11-19 20:47:25 +0000

    Updates

---
+++ precache-manifest.a96c993dc34b924cd5ac9dff043bad06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3836d7feee64ca1c37bd068ba684d5",
+    "url": "/ReactQuiz/index.html"
+  },
+  {
+    "revision": "a778c96334cf8c0c8a9e",
+    "url": "/ReactQuiz/static/css/main.ff03dfc2.chunk.css"
+  },
+  {
+    "revision": "8aeefaf44de37fc99ae1",
+    "url": "/ReactQuiz/static/js/2.aef3f19f.chunk.js"
+  },
+  {
+    "revision": "a778c96334cf8c0c8a9e",
+    "url": "/ReactQuiz/static/js/main.c7a81b0e.chunk.js"
+  },
+  {
+    "revision": "3d11e96eafb6705ae07a",
+    "url": "/ReactQuiz/static/js/runtime-main.deb85331.js"
+  }
+]);
